f33e8e50e425aa7d8191fbc040b710ea0989767e
moscalenco.iulia@gmail.com <aspaRagus45^!>
2021-02-16 16:16:27 +0200

    Upload file

--- src/main/java/com/example/UploadingFilesApplication.java
+++ src/main/java/com/example/UploadingFilesApplication.java
@@ -1,13 +1,28 @@
 package com.example;
 
+import com.example.storage.StorageProperties;
+import com.example.storage.StorageService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
 public class UploadingFilesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UploadingFilesApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner init(StorageService storageService) {
+		return args -> {
+			storageService.deleteAll();
+			storageService.init();
+		};
 	}
 
 }
--- src/test/java/com/example/UploadingFilesApplicationTests.java
+++ src/test/java/com/example/UploadingFilesApplicationTests.java
@@ -1,13 +1,64 @@
 package com.example;
 
+import com.example.storage.StorageFileNotFoundException;
+import com.example.storage.StorageService;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.web.servlet.MockMvc;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@AutoConfigureMockMvc
 @SpringBootTest
 class UploadingFilesApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private MockMvc mockMvc;
 
+    @MockBean
+    private StorageService storageService;
+
+    @Test
+    public void shouldListAllFiles() throws Exception {
+        given(storageService.loadAll())
+                .willReturn(Stream.of(Paths.get("file1.txt"), Paths.get("file2.txt")));
+
+        this.mockMvc.perform(get("/")).andExpect(status().isOk())
+                .andExpect(model().attribute("files",
+                        Matchers.contains("http://localhost/files/file1.txt",
+                                "http://localhost/files/file2.txt")));
+    }
+
+    @Test
+    public void shouldSaveUploadedFile() throws Exception {
+        MockMultipartFile file = new MockMultipartFile("file", "test.txt", "text/plain", "Spring Upload Test".getBytes());
+
+        this.mockMvc.perform(multipart("/").file(file))
+                .andExpect(status().isFound())
+                .andExpect(header().string("Location", "/"));
+
+        then(this.storageService).should().store(file);
+
+    }
+
+    @Test
+    public void should404WhenMissongFile() throws Exception {
+        given(this.storageService.loadAsResource("test.txt"))
+                .willThrow(StorageFileNotFoundException.class);
+
+        this.mockMvc.perform(get("/files/test/txt")).andExpect(status().isNotFound());
+    }
 }
